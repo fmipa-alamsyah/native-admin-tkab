9513697b4d942657657f267831ed228b44a00e51
kusin <fmipa.alamsyah@gmail.com>
2025-10-18 03:15:00 +0700

    -

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PT. Teknologi Karya Anak Bangsa</title>
+
+    <link rel="icon" href="adminlte/dist/img/laravel.png" type="image/png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <link rel="stylesheet" href="adminlte/plugins/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a href="#" class="nav-link text-dark" data-widget="pushmenu" role="button">
+                        <i class="fas fa-bars"></i>
+                    </a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link text-dark">Menu 1</a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link text-dark">Menu 2</a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link text-dark">Menu 3</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link text-dark">
+                        Aryajaya Alamsyah (Web Developer)
+                    </a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link" data-widget="fullscreen" role="button">
+                        <i class="fa-solid fa-maximize"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+
+        <aside class="main-sidebar sidebar-dark-danger elevation-0">
+            <div class="sidebar">
+                <div class="user-panel mt-3 mb-3 pb-3 d-flex">
+                    <div class="image">
+                        <img src="adminlte/dist/img/laravel.png" class="img-fluid elevation-0">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block text-light">TKAB Apps</a>
+                    </div>
+                </div>
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link active">
+                                <i class="nav-icon fa-solid fa-house"></i>
+                                <p class="text-light">Main Dashboard</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fa-solid fa-user-graduate"></i>
+                                <p class="text-light">Data Karyawan</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fa-solid fa-user-graduate"></i>
+                                <p class="text-light">Data Mitra Kerja</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fa-solid fa-book"></i>
+                                <p class="text-light">IT Konsultan & Produk</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fa-solid fa-book"></i>
+                                <p class="text-light">Pelatihan & Sertifikasi</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fa-solid fa-gear"></i>
+                                <p class="text-light">Pengaturan Sistem<i class="fas fa-angle-left right"></i></p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        <i class="fa-solid fa-minus nav-icon"></i>
+                                        <p>Menu 1</p>
+                                    </a>
+                                </li>
+                            </ul>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        <i class="fa-solid fa-minus nav-icon"></i>
+                                        <p>Menu 2</p>
+                                    </a>
+                                </li>
+                            </ul>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        <i class="fa-solid fa-minus nav-icon"></i>
+                                        <p>Menu 3</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fa-solid fa-right-to-bracket"></i>
+                                <p class="text-light">Pelatihan & Sertifikasi</p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </aside>
+
+        <div class="content-wrapper">
+            <div class="content pt-3 pb-3">
+                <div class="container-fluid">
+                    <div class="row mb-4">
+                        <div class="col-sm-12">
+                            <div class="callout callout-success">
+                                <span class="text-dark"><i class="fa-solid fa-book mr-2"></i>Informasi Keuangan PT. Teknologi Karya Anak Bangsa</span>
+                            </div>
+                        </div>
+                        <div class="col-sm-12">
+                            <div class="small-box bg-success">
+                                <div class="inner">
+                                    <h3>RP. 1.000.000.000,-</h3>
+                                    <p class="text-light">Saldo Perusahaan</p>
+                                </div>
+                                <div class="icon"><i class="fa-solid fa-wallet"></i></div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>RP. 1.000.000.000,-</h3>
+                                    <p class="text-light">Total Pemasukan</p>
+                                </div>
+                                <div class="icon"><i class="fa-solid fa-money-check-dollar"></i></div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>RP. 0,-</h3>
+                                    <p class="text-light">Total Pengeluaran</p>
+                                </div>
+                                <div class="icon"><i class="fa-solid fa-money-check-dollar"></i></div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>RP. 0,-</h3>
+                                    <p class="text-light">Total Piutang Usaha</p>
+                                </div>
+                                <div class="icon"><i class="fa-solid fa-money-check-dollar"></i></div>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>RP. 0,-</h3>
+                                    <p class="text-light">Total Hutang Usaha</p>
+                                </div>
+                                <div class="icon"><i class="fa-solid fa-money-check-dollar"></i></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mb-4">
+                        <div class="col-sm-12">
+                            <div class="callout callout-success">
+                                <span class="text-dark"><i class="fa-solid fa-book mr-2"></i>Data-Data Administrasi</span>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>1</h3>
+                                    <p class="text-light">Data P</p>
+                                </div>
+                                <div class="icon"><i class="fa-solid fa-book"></i></div>
+                                <a href="#" class="small-box-footer"><i class="fa-solid fa-clone mr-1"></i> Selengkapnya</a>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>2</h3>
+                                    <p class="text-light">Data Q</p>
+                                </div>
+                                <div class="icon"><i class="fa-solid fa-book"></i></div>
+                                <a href="#" class="small-box-footer"><i class="fa-solid fa-clone mr-1"></i> Selengkapnya</a>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>3</h3>
+                                    <p class="text-light">Data R</p>
+                                </div>
+                                <div class="icon"><i class="fa-solid fa-book"></i></div>
+                                <a href="#" class="small-box-footer"><i class="fa-solid fa-clone mr-1"></i> Selengkapnya</a>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="small-box bg-info">
+                                <div class="inner">
+                                    <h3>4</h3>
+                                    <p class="text-light">Data S</p>
+                                </div>
+                                <div class="icon"><i class="fa-solid fa-book"></i></div>
+                                <a href="#" class="small-box-footer"><i class="fa-solid fa-clone mr-1"></i> Selengkapnya</a>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <footer class="main-footer">
+            <div class="float-right d-none d-sm-inline-block text-dark">
+                Version 1.0
+            </div>
+            <div class="float-left d-none d-sm-inline-block text-dark">
+                Copyright &copy; 2025. PT. Teknologi Karya Anak Bangsa. All rights reserved.
+            </div>
+            <div class="clearfix"></div>
+        </footer>
+
+    </div>
+    <!-- ./wrapper -->
+
+    <script src="adminlte/plugins/jquery/jquery.min.js"></script>
+    <script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="adminlte/plugins/fontawesome/js/all.min.js"></script>
+    <script src="adminlte/dist/js/adminlte.min.js"></script>
+</body>
+
+</html>
